d85487e13c5b0ac103de29a062daf559fc02dac4
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-03 21:27:06 +0100

    Booleans and comparisons

---
+++ 07_booleans/code.py
@@ -0,0 +1,11 @@
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)
+
+
+friends = ["bob", "rolf", "Anne"]
+list = ["bob", "rolf", "Anne"]
+
+print(friends == list)
+print(friends is list) # Exactly the same thing even space in memory
+print(friends is friends) # Exactly the same thing even space in memory
