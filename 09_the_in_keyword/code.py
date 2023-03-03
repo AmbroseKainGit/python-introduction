5353d50174f08b64e75595dea061ef002895712d
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-03 21:59:19 +0100

    If and In exercises

---
+++ 09_the_in_keyword/code.py
@@ -0,0 +1,6 @@
+friends = ["Rolf", "bob", "jen"]
+print("Jen" in friends)
+
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter something youve watched recently: ")
+print(user_movie in movies_watched)
