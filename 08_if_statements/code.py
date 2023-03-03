ecadf1a9d9ace6a4f9dde967299f7d27719c0982
Cristian Ceballos <cristian.ceballos@precompro.com>
2023-03-03 21:45:42 +0100

    If statemets

---
+++ 08_if_statements/code.py
@@ -0,0 +1,7 @@
+day_of_week = input("What day of the wee kis it today? ").lower()
+if day_of_week == "monday":
+    print('IS MONDAY')
+elif day_of_week == "tuesday":
+    print('IS TUESDAY')
+else:
+    print('IS NOT MONDAY')
